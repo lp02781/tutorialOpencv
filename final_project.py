8b4c52e0b3b64e12965c22f2ae9456ed4e77aa44
Fikih Muhamad <lp02781@gmail.com>
2022-05-11 22:36:57 +0900

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -6,20 +6,24 @@ import numpy
 from skimage import color
 import numpy as np
 from pyefd import elliptic_fourier_descriptors
-
+import pyefd
 green_retangle=[61, 99, 220, 255, 128, 176]
 blue_triangle= [43, 127, 0, 210, 125, 196]
 orange_hexagon= [0, 61, 69, 210, 184, 251]
 red_circle= [0, 12, 76, 255, 255, 255]
 purple_square= [132, 173, 23, 238, 123, 251]
 yellow_pentagon= [9, 182, 80, 255, 255, 255]
-color = orange_hexagon
+color = yellow_pentagon
 hL = color[0];
 hH = color[1];
 sL = color[2]; 
 sH = color[3];
 vL = color[4];
 vH = color[5];
+def efd_feature(contour):
+    coeffs = elliptic_fourier_descriptors(
+        contour, order=10, normalize=True)
+    return coeffs.flatten()[3:]
 LowerRegion = np.array([hL,sL,vL],np.uint8)
 upperRegion = np.array([hH,sH,vH],np.uint8)
 img = cv2.imread('wkwk.png')
@@ -37,12 +41,11 @@ img = cv2.Canny(img,100,200,5)
 cv2.imshow('raw', img)
 contours, hierarchy = cv2.findContours(
     img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#coeffs = []
-pixel = []
+coeffs = np.empty([80])
 for cnt in contours:
-    coeffs = []
-    coeffs.append(elliptic_fourier_descriptors(
-        numpy.squeeze(cnt), order=10))
+    #coeffs = []
+    np.append(coeffs,(elliptic_fourier_descriptors(
+        numpy.squeeze(cnt), order=10)))
     count = len(np.squeeze(cnt))
     epsilon1 = 0.01*cv2.arcLength(cnt, True)          
     approx1 = cv2.approxPolyDP(cnt, epsilon1, True)
@@ -64,6 +67,11 @@ for cnt in contours:
         text="Hexagon"
     else:
         text="Circle"
+#print(coeffs)
+B = np.reshape(coeffs, (-1, 4))
+print(B)
+#bagi tiap 4 angka
+pyefd.plot_efd(B, locus=(0.0, 0.0), image=None, contour=None, n=300)
 cv2.circle(haha, (cX, cY), 5, (0, 0, 255), 1)
 cv2.putText(haha, text, (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 5, 100), 2)
 cv2.imshow('process', haha)
